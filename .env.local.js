48360a65077f703ba8dbb0a49bfd725c59a8f2df
Marek Murawski <136197278+Dai-Kaio@users.noreply.github.com>
2025-04-10 23:55:57 +0200

    First push
    Uploading a base version of app

---
+++ .env.local.js
@@ -0,0 +1,4 @@
+MONGODB_URI=mongodb+srv://lnaticart:n1SKSnLzAEmILBk5@cluster0.uwud9k.mongodb.net/outreach?retryWrites=true&w=majority
+NEXTAUTH_SECRET=jakislosowydlugistring123456789
+NEXTAUTH_URL=https://staging.mrvsky.pro
+NODE_ENV=production
